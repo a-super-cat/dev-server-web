1889cf532bf64ca18cf11091ccdb4ef1f14c572f
贾志伟 <2405015284@qq.com>
2024-03-23 21:20:09 +0800

    feat: 删除不必要的依赖以及调整package包含的内容

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import { createI18n } from 'vue-i18n'
 
 import App from './App.vue'
@@ -23,7 +22,6 @@ const i18n = createI18n<false>({
   }
 })
 
-app.use(createPinia())
 app.use(router)
 app.use(i18n)
 
